c427ce4af0487911fb2c1a59ec7799b29af4d710
Shrinivas Kalyani <shrinivaskalyani05@gmail.com>
2021-05-18 20:43:59 +0530

    Added SignupView in users app

--- users/views.py
+++ users/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework import generics
+from rest_framework.response import Response
 
-# Create your views here.
+from users.serializers import SignupSerializer
+
+
+class SignupView(generics.CreateAPIView):
+
+    serializer_class = SignupSerializer
+
+    def create(self, request, *args, **kwargs):
+        request.data.update({'ip_address': request.META['REMOTE_ADDR']})
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        response = {
+            'email': serializer.data['email'],
+            'first_name': serializer.data['first_name'],
+            'last_name': serializer.data['last_name'],
+        }
+        return Response(
+            response,
+            status=status.HTTP_201_CREATED,
+            headers=headers
+        )
+
+
+signup_view = SignupView.as_view()
