6eff5125fe17cf7af1c824fa79bfd690664f9d44
Shrinivas Kalyani <shrinivaskalyani05@gmail.com>
2021-05-18 17:14:56 +0530

    Added User model in user app

--- users/models.py
+++ users/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+
+    ip_address = models.GenericIPAddressField()
+
+    country = models.CharField(
+        max_length=50,
+    )
+
+    country_code = models.CharField(
+        max_length=5,
+    )
+
+    country_geoname_id = models.IntegerField()
+
+    longitude = models.FloatField()
+
+    latitude = models.FloatField()
+
+    joined_on_holiday = models.BooleanField(
+        default=False,
+    )
