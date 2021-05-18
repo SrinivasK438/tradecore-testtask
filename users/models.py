916f44cce287a26605f354bb57266965929bd6d4
Shrinivas Kalyani <shrinivaskalyani05@gmail.com>
2021-05-18 18:02:40 +0530

    Updated User model in users app

--- users/models.py
+++ users/models.py
@@ -4,22 +4,50 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
 
-    ip_address = models.GenericIPAddressField()
+    username = None
+
+    email = models.EmailField(
+        unique=True
+    )
+
+    ip_address = models.GenericIPAddressField(
+        null=True,
+        blank=True,
+    )
 
     country = models.CharField(
         max_length=50,
+        null=True,
+        blank=True,
     )
 
     country_code = models.CharField(
         max_length=5,
+        null=True,
+        blank=True,
     )
 
-    country_geoname_id = models.IntegerField()
+    country_geoname_id = models.IntegerField(
+        null=True,
+        blank=True,
+    )
 
-    longitude = models.FloatField()
+    longitude = models.FloatField(
+        null=True,
+        blank=True,
+    )
 
-    latitude = models.FloatField()
+    latitude = models.FloatField(
+        null=True,
+        blank=True,
+    )
 
     joined_on_holiday = models.BooleanField(
         default=False,
     )
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
