54828f8800120c41306c18534cc4d3a98b9bd17f
Shrinivas Kalyani <shrinivaskalyani05@gmail.com>
2021-05-19 11:49:21 +0530

    Added Post model in posts app

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+from django.conf import settings
+
+
+class Post(models.Model):
+
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='posts',
+    )
+
+    title = models.CharField(
+        max_length=150
+    )
+
+    content = models.TextField()
+
+    posted_on = models.DateTimeField(
+        auto_now_add=True,
+    )
+
+    like = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name='liked_posts',
+        blank=True,
+    )
+
+    def __str__(self):
+        return '"{title}" by {author}'.format(
+            title=self.title,
+            author=self.author,
+        )
